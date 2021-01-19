9255c717dd95dc31e6f08da4bc67db2af05e2ed7
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-01-19 09:35:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ function downloadFromInfoCallback(text, lang, speed, stream){
  * @param {Language} lang
  * @param {number} speed
  */
-function getVoiceStream(text, lang="en-GB", speed=1){
-    const stream=new Stream.PassThrough();
-    downloadFromInfoCallback(text,lang,speed,stream);
+function getVoiceStream(text, lang="fr-FR", speed=1){
+    const stream = new Stream.PassThrough();
+    downloadFromInfoCallback(text, lang ,speed, stream);
     return stream;
 }
 
@@ -49,13 +49,13 @@ function getVoiceStream(text, lang="en-GB", speed=1){
  * @param {Language} lang
  * @param {number} speed
  */
-function saveToFile(filePath, text, lang="en-GB", speed=1){
-    const stream=new Stream.PassThrough();
-    const writeStream=fs.createWriteStream(filePath);
-    downloadFromInfoCallback(text,lang,speed,stream);
+function saveToFile(filePath, text, lang="fr-FR", speed=1){
+    const stream = new Stream.PassThrough();
+    const writeStream = fs.createWriteStream(filePath);
+    downloadFromInfoCallback(text, lang, speed, stream);
     stream.pipe(writeStream);
-    stream.on("end",()=>writeStream.close());
+    stream.on("end",() => writeStream.close());
 }
 
-module.exports.getVoiceStream=getVoiceStream;
-module.exports.saveToFile=saveToFile;
+module.exports.getVoiceStream = getVoiceStream;
+module.exports.saveToFile = saveToFile;
